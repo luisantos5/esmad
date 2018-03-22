e86005a70c9b2aff04b7a54fc0b8d498d25489b1
Luís Santos <luis.costasantos@outlook.com>
2018-03-22 15:27:01 +0000

    Início Ficha 3

---
+++ Ficha_03/index.js
@@ -0,0 +1,34 @@
+function funcA(){
+    
+
+}
+function funcB(){
+    let nome = prompt("Indique o seu nome: ")
+    let apelido = prompt("Indique o seu Apelido: ")
+    console.log("O seu nome completo é: "+ nome + " " + apelido)
+
+}
+function funcC(){
+    
+
+}
+function funcD(){
+    
+
+}
+function funcE(){
+    
+
+}
+function funcF(){
+    
+
+}
+function funcG(){
+    
+
+}
+function funcH(){
+    
+
+}
